9c6eb9adc5f12091a43efa3b85a72c72138e4257
Liam T O'Toole <lotoole@uvm.edu>
2018-09-04 10:53:25 -0400

    added an event listener to the button

--- app/src/main/java/com/example/liamo/firstapp/MainActivity.java
+++ app/src/main/java/com/example/liamo/firstapp/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.liamo.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "Simplest";
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = (Button) findViewById(R.id.my_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick");
+
+            }
+        });
     }
 }
